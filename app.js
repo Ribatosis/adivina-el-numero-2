a91f4320eef9c6a43ff879af0dfcf54de5e67058
Ribatosis <juliana.espinosa@genstudents.org>
2025-01-31 22:32:18 -0500

    Nueva opción: reiniciar juego

--- app.js
+++ app.js
@@ -11,25 +11,42 @@ nombresCosas('h1', "Adivina el número secreto");
 nombresCosas('p', "ingresa un número entre el 1 y el 10")
 
 function intentoUsuario(){
+
     let verificacionUser = parseInt(document.getElementById('inputValidacion').value);
    
     if(verificacionUser == numeroSecreto){
         nombresCosas('p', `Acertaste el número en: ${contador} ${(contador == 1) ? ' intento' : ' intentos'}`);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
     }else{
         if(verificacionUser > numeroSecreto){
-            nombresCosas('p', 'El número secreto es menor')
-            contador++;
+            nombresCosas('p', 'El número secreto es menor')  
         }else if(verificacionUser < numeroSecreto){
             nombresCosas('p', 'El número secreto es mayor')
-            contador++;
         }
-        
+        contador++;
+        limpiarCaja();
     }
 
 
+}
+
+function limpiarCaja(){
+    let cajaVacia = document.getElementById('inputValidacion').value = '';
 }
 
 function numeroAleatorio (min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function reiniciarJuego(){
+    nombresCosas('p', "ingresa un número entre el 1 y el 10")
+    numeroSecreto = numeroAleatorio(1, 10);
+    contador = 1;
+    limpiarCaja()
+
+    document.getElementById('reiniciar').setAttribute('disabled', 'true')
+
+    verificacionUser()
+}
